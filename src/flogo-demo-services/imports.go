fbebd55d7ed53165606d59e9e255b9ffb0872ac5
Mathieu Debove <mathieu.debove@sqits.net>
2018-11-13 17:08:41 +0100

    Initial implementation

---
+++ src/flogo-demo-services/imports.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	_ "github.com/TIBCOSoftware/flogo-contrib/action/flow"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/actreply"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/rest"
+	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/rest"
+	_ "github.com/debovema/flogo-opentracing-listener"
+	_ "github.com/square-it/flogo-contrib-activities/command"
+	_ "github.com/square-it/flogo-contrib-activities/copyfile"
+)
